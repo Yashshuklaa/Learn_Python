87ec15dc6a9d633723bd552b208c8b74bebafd3c
Yashshuklaa <yashshukla.cmunafa@gmail.com>
2017-01-10 19:22:57 +0530

    added packing and Unpacking concepts

--- first.py
+++ first.py
@@ -1,2 +1,21 @@
 
 print "this is my first python program"
+
+
+# Variable & Unpacking Example
+
+my_variable = [1,4.2,True]
+int,float,bool = my_variable
+print type(int)
+print type (float)
+print type(bool)
+
+'''And also we are using Type for recognize the value Type'''
+#Packing 
+def varibale(int,float,bool):
+	print int,float,bool
+arg = 1,4.2,True
+varibale(*arg)
+
+	
+
